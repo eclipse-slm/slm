a6866cb46fa9a33bbb18e5590d53031b6ba7114c
ipa-big <benjamin.goetz@ipa.fraunhofer.de>
2023-06-08 21:04:36 +0200

    init

---
+++ common/common.consul/common.consul.client/src/main/java/org/eclipse/slm/common/consul/client/apis/ConsulAgentApiClient.java
@@ -0,0 +1,77 @@
+package org.eclipse.slm.common.consul.client.apis;
+
+import com.google.common.net.HostAndPort;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.model.agent.ImmutableRegistration;
+import com.orbitz.consul.model.catalog.ImmutableCatalogRegistration;
+import com.orbitz.consul.model.health.ImmutableService;
+import org.eclipse.slm.common.consul.client.ConsulCredential;
+import org.eclipse.slm.common.consul.model.catalog.Node;
+import org.eclipse.slm.common.consul.model.exceptions.ConsulLoginFailedException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ConsulAgentApiClient extends AbstractConsulApiClient {
+    private final ConsulNodesApiClient consulNodesApiClient;
+
+    public ConsulAgentApiClient(
+            @Value("${consul.scheme}")      String consulScheme,
+            @Value("${consul.host}")        String consulHost,
+            @Value("${consul.port}")        int consulPort,
+            @Value("${consul.acl-token}")   String consulToken,
+            @Value("${consul.datacenter}")  String consulDatacenter,
+            ConsulNodesApiClient consulNodesApiClient
+    ) {
+        super(consulScheme, consulHost, consulPort, consulToken, consulDatacenter);
+        this.consulNodesApiClient = consulNodesApiClient;
+    }
+
+    public void registerService(
+            ConsulCredential consulCredential,
+            UUID nodeId,
+            String serviceName,
+            UUID serviceId,
+            Optional<Integer> servicePort,
+            List<String> serviceTags,
+            Map<String, String> serviceMetaData
+    ) throws ConsulLoginFailedException {
+        ImmutableRegistration newCatalogRegistration = ImmutableRegistration.builder()
+                .setName(serviceName)
+                .setId(serviceId.toString())
+                .setMeta(serviceMetaData)
+                .setTags(serviceTags)
+                .setPort(servicePort.orElse(0))
+                .build();
+
+        try {
+            getConsulAgentClient(consulCredential, nodeId)
+                    .agentClient()
+                    .register(newCatalogRegistration);
+        } catch (NullPointerException e) {
+            LOG.error("Failed to register service because Node not found: " + e.getMessage());
+        }
+    }
+
+    private Consul getConsulAgentClient(
+            ConsulCredential consulCredential,
+            UUID nodeId
+    ) throws ConsulLoginFailedException {
+        Optional<Node> optionalNode = consulNodesApiClient.getNodeById(
+                consulCredential,
+                nodeId
+        );
+
+        if(optionalNode.isEmpty())
+            return null;
+
+        Node node = optionalNode.get();
+
+        return Consul.builder()
+                .withHostAndPort(HostAndPort.fromParts(node.getAddress(),9500))
+                .build();
+    }
+}
