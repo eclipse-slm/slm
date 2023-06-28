8d88e1dcd103f094bc1ac4e71186564a2feb16a9
big <big@ipa.fhg.de>
2023-06-28 19:05:25 +0200

    add lookup of consul token

--- notification_service/notification_service.service/notification_service.service.rest/src/main/docker/startup.sh
+++ notification_service/notification_service.service/notification_service.service.rest/src/main/docker/startup.sh
@@ -13,6 +13,14 @@ until curl -m 5 -s --location --request GET "$KEYCLOAK_AUTHSERVERURL/auth/realms
   sleep 1
 done
 
+# Wait until Consul configuration is present
+CONSUL_TOKEN_FILE="/app/consul/consul_token"
+until [ -f "$CONSUL_TOKEN_FILE" ]; do
+  echo "Consul token config file '$CONSUL_TOKEN_FILE' missing -> sleeping"
+  sleep 3
+done
+export CONSUL_ACLTOKEN=$(cat "$CONSUL_TOKEN_FILE")
+
 # Wait until AWX configuration is present
 AWX_CONFIG_DIRECTORY="/app/awx/"
 while [ -z "$(ls -A $AWX_CONFIG_DIRECTORY)" ]; do
