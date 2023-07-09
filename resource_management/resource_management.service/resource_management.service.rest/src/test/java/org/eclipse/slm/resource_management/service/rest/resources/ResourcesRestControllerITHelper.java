3a6cfff81d181747ec4e1b3f8258294761d1b317
big <big@ipa.fhg.de>
2023-07-09 22:34:58 +0200

    disable component

--- resource_management/resource_management.service/resource_management.service.rest/src/test/java/org/eclipse/slm/resource_management/service/rest/resources/ResourcesRestControllerITHelper.java
+++ resource_management/resource_management.service/resource_management.service.rest/src/test/java/org/eclipse/slm/resource_management/service/rest/resources/ResourcesRestControllerITHelper.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Component
+//@Component
 public class ResourcesRestControllerITHelper {
 
     @Autowired
