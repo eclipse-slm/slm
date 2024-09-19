72e97a3ea9a63fb1cef3dee07e83d8fef813d4ed
Ilgmi <michael.ilg@mailbox.org>
2024-09-19 15:46:23 +0200

    merge develop

--- ui/src/components/resources/ResourceSubmodels.vue
+++ ui/src/components/resources/ResourceSubmodels.vue
@@ -124,7 +124,7 @@ export default {
       })
     },
     getAasDescriptor() {
-      AasRestApi.getResourceAasDescriptor(this.resourceId).then(response => {
+      ResourceManagementClient.aasApi.getResourceAASDescriptors(this.resourceId).then(response => {
         this.aasDescriptor = response
       }).catch((e) => {
         console.log(e)
