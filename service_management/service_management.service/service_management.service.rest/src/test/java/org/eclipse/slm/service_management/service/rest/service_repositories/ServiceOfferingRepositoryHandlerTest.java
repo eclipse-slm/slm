b4de5c8d0648f1939182b5f9543fffb56b73a78e
Matthias Schneider <matthias.schneider@ipa.fraunhofer.de>
2025-09-25 10:40:39 +0200

    Fix vault image tag and version

--- service_management/service_management.service/service_management.service.rest/src/test/java/org/eclipse/slm/service_management/service/rest/service_repositories/ServiceOfferingRepositoryHandlerTest.java
+++ service_management/service_management.service/service_management.service.rest/src/test/java/org/eclipse/slm/service_management/service/rest/service_repositories/ServiceOfferingRepositoryHandlerTest.java
@@ -42,7 +42,7 @@ public class ServiceOfferingRepositoryHandlerTest {
     private ServiceRepositoryHandler serviceRepositoryHandler;
 
     @Container
-    private final GenericContainer<?> vaultContainer = new GenericContainer<>(DockerImageName.parse("vault:1.11.0"))
+    private final GenericContainer<?> vaultContainer = new GenericContainer<>(DockerImageName.parse("hashicorp/vault:1.20.3"))
             .withExposedPorts(8200)
             .withEnv("VAULT_DEV_ROOT_TOKEN_ID", VAULT_TOKEN);
 
