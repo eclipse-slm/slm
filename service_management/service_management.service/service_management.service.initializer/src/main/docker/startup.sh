f4f4e6ed10d099d40a72b428b09f411fcea50feb
Matthias Schneider <matthias.schneider@ipa.fraunhofer.de>
2026-02-16 17:48:32 +0100

    Fix POMs and change to fabric8 Maven Docker Plugin

--- service_management/service_management.service/service_management.service.initializer/src/main/docker/startup.sh
+++ service_management/service_management.service/service_management.service.initializer/src/main/docker/startup.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
 
+set -euo pipefail
+
+COMMON_SH="/app/common.sh"
+source "$COMMON_SH"
+
 echo "Init Directory: $SERVICEMANAGEMENT_INITDIRECTORIES"
 
-# Wait until Service Management is running
-until curl -m 5 -s -k --location --request GET "$SERVICEMANAGEMENT_URL/v3/api-docs" > /dev/null; do
-  echo "Service Management is unavailable -> sleeping"
-  sleep 1
-done
-
-# Wait until Keycloak is running
-until curl -m 5 -s -k --location --request GET "$KEYCLOAK_AUTHSERVERURL/realms/$KEYCLOAK_REALM/.well-known/openid-configuration" > /dev/null; do
-  echo "Keycloak is unavailable -> sleeping"
-  sleep 1
-done
-
-# Start App
-java -jar -Djava.security.egd=file:/dev/./urandom /app/app.jar
+wait_for_keycloak
+wait_for_service_management
+start_app
