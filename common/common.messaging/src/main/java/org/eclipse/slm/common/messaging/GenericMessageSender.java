2d10acf3cb5a8dd41ac5fef59c0dc909b20566e1
Matthias Schneider <matthias.schneider@ipa.fraunhofer.de>
2026-03-12 17:36:43 +0100

    Fix message serialization

--- common/common.messaging/src/main/java/org/eclipse/slm/common/messaging/GenericMessageSender.java
+++ common/common.messaging/src/main/java/org/eclipse/slm/common/messaging/GenericMessageSender.java
@@ -3,6 +3,7 @@ package org.eclipse.slm.common.messaging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +15,7 @@ public abstract class GenericMessageSender<T extends AbstractEventMessage> {
 
     public GenericMessageSender(RabbitTemplate rabbitTemplate) throws Exception {
         this.rabbitTemplate = rabbitTemplate;
+        this.rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
     }
 
     public void sendMessage(T message) {
