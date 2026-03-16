35e47ddfabf064a0fea57de13febc514b1cb1f94
Matthias Schneider <matthias.schneider@ipa.fraunhofer.de>
2026-03-16 10:20:27 +0100

    Add annotations for pre authorization

---
+++ common/common.restserver/src/main/java/org/eclipse/slm/common/restserver/annotations/AuthorizedAsSlmUser.java
@@ -0,0 +1,17 @@
+package org.eclipse.slm.common.restserver.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.*;
+
+/**
+ * Custom annotation to check if the user has either the 'slm-user' role.
+ * This can be used on methods or classes to enforce this security requirement.
+ */
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@PreAuthorize("hasRole('slm-user')")
+public @interface AuthorizedAsSlmUser {
+}
