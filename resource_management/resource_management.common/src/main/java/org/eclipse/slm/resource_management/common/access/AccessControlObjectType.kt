ffde8f5ce4452beb7a820ace5cab34e4d2059f21
Matthias <matthias@online-schneider.net>
2026-09-16 23:13:38 +0000

    refactor(access): make AccessControlObjectType a String constant holder

--- resource_management/resource_management.common/src/main/java/org/eclipse/slm/resource_management/common/access/AccessControlObjectType.kt
+++ resource_management/resource_management.common/src/main/java/org/eclipse/slm/resource_management/common/access/AccessControlObjectType.kt
@@ -1,7 +1,11 @@
 package org.eclipse.slm.resource_management.common.access
 
-enum class AccessControlObjectType {
-    RESOURCE,
-    CAPABILITY_SERVICE,
-    REMOTE_ACCESS
+/**
+ * Object types this service registers in [org.eclipse.slm.common.access.AccessControlPolicy].
+ * Values match the strings already stored in the policy_objects.object_type column.
+ */
+object AccessControlObjectType {
+    const val RESOURCE = "RESOURCE"
+    const val CAPABILITY_SERVICE = "CAPABILITY_SERVICE"
+    const val REMOTE_ACCESS = "REMOTE_ACCESS"
 }
